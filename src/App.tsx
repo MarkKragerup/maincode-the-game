2b53ddef810ea5de6a58e936e5e89d50ee490466
Loui Nissen-Petersen <59645750+ItzLue@users.noreply.github.com>
2021-03-07 09:10:00 +0100

    animation character

--- src/App.tsx
+++ src/App.tsx
@@ -17,12 +17,20 @@ enum EMap {
     lab = 'LAB'
 }
 
+enum EFaceDirection {
+    up = 'face-up',
+    down = 'face-down',
+    right = 'face-right',
+    left = 'face-left'
+}
+
 const App = () => {
     const terminalDoorPosition: IPosition = {x: 7, y: 0};
     const labDoorPosition: IPosition = {x: 2, y: 0};
 
     const [currentPos, setCurrentPos] = useState<IPosition>({x: 5, y: 5});
     const [currentMap, setCurrentMap] = useState<EMap>(EMap.entrance);
+    const [faceDirection, setFaceDirection] = useState<EFaceDirection>(EFaceDirection.up)
 
     const [shouldOpenTerminal, setShouldOpenTerminal] = useState(false);
 
@@ -34,10 +42,22 @@ const App = () => {
     useEffect(() => {
         // If terminal is open do not listen for key press
         if (!shouldOpenTerminal) {
-            if (forwardPress && isValidMove(entrance, currentPos, 'w')) setCurrentPos({x: currentPos.x, y: currentPos.y - 1});
-            if (backwardPress && isValidMove(entrance, currentPos, 's')) setCurrentPos({x: currentPos.x, y: currentPos.y + 1});
-            if (leftPress && isValidMove(entrance, currentPos, 'a')) setCurrentPos({x: currentPos.x - 1, y: currentPos.y});
-            if (rightPress && isValidMove(entrance, currentPos, 'd')) setCurrentPos({x: currentPos.x + 1, y: currentPos.y});
+            if (forwardPress && isValidMove(entrance, currentPos, 'w')) {
+                setCurrentPos({x: currentPos.x, y: currentPos.y - 1})
+                setFaceDirection(EFaceDirection.up)
+            }
+            if (backwardPress && isValidMove(entrance, currentPos, 's')) {
+                setCurrentPos({x: currentPos.x, y: currentPos.y + 1})
+                setFaceDirection(EFaceDirection.down)
+            }
+            if (leftPress && isValidMove(entrance, currentPos, 'a')) {
+                setCurrentPos({x: currentPos.x - 1, y: currentPos.y})
+                setFaceDirection(EFaceDirection.left)
+            }
+            if (rightPress && isValidMove(entrance, currentPos, 'd')) {
+                setCurrentPos({x: currentPos.x + 1, y: currentPos.y})
+                setFaceDirection(EFaceDirection.right)
+            }
         }
 
         // eslint-disable-next-line
@@ -55,39 +75,47 @@ const App = () => {
     }
 
     const terminal = (
-      <div className='terminal'>
-          <div className='title-bar'>
-              <div className='terminal-text-layout'>
-                  <p className='terminal-heading white-text'>MAINCODE</p>
-                  <p className='white-text thick-text'><b>COMMANDS</b></p>
-                  <ul className='command-list white-text'>
-                      <li className='li-item'>Valid command 1</li>
-                      <li className='li-item'>Valid command 2</li>
-                      <li className='li-item'>Valid command 3</li>
-                      <li className='li-item'>Valid command 4</li>
-                  </ul>
-                  <br/>
-                  <span className="">{'>'}</span>
-                  <input autoFocus className='input'/>
-              </div>
-          </div>
-      </div>
+        <div className='terminal'>
+            <div className='title-bar'>
+                <div className='terminal-text-layout'>
+                    <p className='terminal-heading white-text'>MAINCODE</p>
+                    <p className='white-text thick-text'><b>COMMANDS</b></p>
+                    <ul className='command-list white-text'>
+                        <li className='li-item'>Valid command 1</li>
+                        <li className='li-item'>Valid command 2</li>
+                        <li className='li-item'>Valid command 3</li>
+                        <li className='li-item'>Valid command 4</li>
+                    </ul>
+                    <br/>
+                    <span className="">{'>'}</span>
+                    <input autoFocus className='input'/>
+                </div>
+            </div>
+        </div>
     );
 
     return (
-        <div className='app'>
+        <div>
             {
                 currentMap === EMap.entrance &&
                 <div className='map'>
-                    {entrance.map((e, i) => <div className='row'>{e.map((tile, j) => RenderTile(tile, i, j))}</div>)}
-                    <div className='avatar' style={{left: `${currentPos.x * 10}%`, top: `${currentPos.y * 10}%`}}/>
+                    {entrance.map((e, i) => <div className='row'>{e.map((tile, j) => RenderTile(tile, j, i))}</div>)}
+                    <div className='character' style={{left: `${currentPos.x * 10}%`, top: `${currentPos.y * 10}%`}}>
+                        <img className={`character-spritesheet ${faceDirection}`}
+                             src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/21542/DemoRpgCharacter.png"
+                             alt="Character"/>
+                    </div>
                 </div>
             }
             {
                 currentMap === EMap.lab &&
                 <div className='map'>
                     {lab.map((e, i) => <div className='row'>{e.map((tile, j) => RenderTile(tile, i, j))}</div>)}
-                    <div className='avatar' style={{left: `${currentPos.x * 10}%`, top: `${currentPos.y * 10}%`}}/>
+                    <div className='character' style={{left: `${currentPos.x * 10}%`, top: `${currentPos.y * 10}%`}}>
+                        <img className={`character-spritesheet ${faceDirection}`}
+                             src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/21542/DemoRpgCharacter.png"
+                             alt="Character"/>
+                    </div>
                 </div>
             }
             <Modal isOpen={shouldOpenTerminal} onCloseCallback={() => setShouldOpenTerminal(false)}>{terminal}</Modal>
