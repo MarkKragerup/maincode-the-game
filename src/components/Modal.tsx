60e9815c0c2f9f558e799092defe76df9be1f6fa
Loui Nissen-Petersen <59645750+ItzLue@users.noreply.github.com>
2021-03-07 02:08:29 +0100

    modal with blurred background

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,22 +1,24 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import './modal.css';
 import closebutton from "../assets/CustomX.svg";
 
 type IProps = {
-  isOpen: boolean;
-  onCloseCallback: () => void;
+    isOpen: boolean;
+    onCloseCallback: () => void;
 }
 
-const Modal: React.FC<IProps> = ({children, isOpen= false, onCloseCallback}): JSX.Element => {
-  return (
-    <div className={`modal-container ${isOpen ? 'block' : 'none'}`}>
-      <div className='close-button' onClick={onCloseCallback}>
-        <img src={closebutton} alt='close' className='cross' height={12} width={12}/>
-      </div>
-      <div className='children-container'>
-        {children}
-      </div>
-    </div>
-  );
+const Modal: React.FC<IProps> = ({children, isOpen = false, onCloseCallback}): JSX.Element => {
+    return (
+        <div className={`modal-background ${isOpen ? 'block' : 'none'}`}>
+            <div className='modal-container'>
+                <div className='close-button' onClick={onCloseCallback}>
+                    <img src={closebutton} alt='close' className='cross' height={12} width={12}/>
+                </div>
+                <div className='children-container'>
+                    {children}
+                </div>
+            </div>
+        </div>
+    );
 }
 export default Modal;
